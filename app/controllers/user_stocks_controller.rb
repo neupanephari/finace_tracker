c75f541ec4f944388a7de6242c00a25f6c958d58
Hari Neupane <neupanephari@gmail.com>
2016-04-13 01:10:53 +0000

    Update views and navigations

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -46,7 +46,7 @@ class UserStocksController < ApplicationController
     respond_to do |format|
       if @user_stock.save
         format.html { redirect_to my_portfolio_path, 
-                    notice: "#{@user_stock.stock.ticker} stock was successfully created!"  }
+                    notice: " Stock#{@user_stock.stock.ticker} was successfully added!"  }
         format.json { render :show, status: :created, location: @user_stock }
       else
         format.html { render :new }
